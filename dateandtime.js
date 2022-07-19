722dceefed2fb8257c9de9073ec130e0d6f664f5
Ken1567 <107854367+Ken1567@users.noreply.github.com>
2022-07-19 14:26:04 +0800

    Update dateandtime.js

--- dateandtime.js
+++ dateandtime.js
@@ -1,9 +1,12 @@
+var date = new Date();
+var n = date.toDateString().split(' ').slice(1,3).join(' ');
+
 const div = document.querySelector('.clock');
 
 const clock = () => {
 const now = new Date();
 const htmlClock = `Today is : ${dateFns.format(now, 'dddd')}.
-Current time is : ${dateFns.format(now, 'hh A : mm : ss')}`;
+Current time is : ${dateFns.format(now, n + ' hh : mm : ss A')}`;
 div.innerHTML = htmlClock;
 };
 
